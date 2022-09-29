113e8c53c623c63d86ad19b8e1b614fd487f7724
Jitsu <60904693+anosu@users.noreply.github.com>
2022-09-29 16:50:49 +0800

    Update api.php

--- api.php
+++ api.php
@@ -2,27 +2,23 @@
 $sort = !empty($_GET['sort']) ? $_GET['sort'] : 'all';
 $num = !empty($_GET['num']) ? $_GET['num'] : 1;
 $type = $_GET['type'];
+$urls = [];
 $filename = './txt/' . $sort . '.txt';
 if (!file_exists($filename)) {
     die('sort为空或文件不存在');
 }
 
 // 从文本获取链接
-$pics = [];
-$fs = fopen($filename, "r");
-while (!feof($fs)) {
-    $line = trim(fgets($fs));
-    if ($line != '') {
-        array_push($pics, $line);
-    }
-}
+$pics = file_get_contents($filename);
+$pics = explode(PHP_EOL, $pics);
+
 // 返回格式和数量
 $type = $num > 1 ? 'json' : $type;
 $num = $num > 100 ? 100 : $num;
 
-$urls = [];
-for ($i = 1; $i <= $num; $i++) {
+for ($i = 0; $i < $num; $i++) {
     $url = $pics[array_rand($pics)];
+    // $url = str_replace(array("\r","\n","\r\n"), '', $url);
     array_push($urls, $url);
 }
 
@@ -32,7 +28,7 @@ switch ($type) {
         //JSON返回
     case 'json':
         header('Content-type:text/json');
-        die(json_encode(['pic' => $urls], JSON_PRETTY_PRINT));
+        die(json_encode(['pics' => $urls], JSON_PRETTY_PRINT));
 
     default:
         die(header("Location: $url"));
