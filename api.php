b4d8fc674e948a4b70de63ef2351f9722b95367c
Jitsu <60904693+anosu@users.noreply.github.com>
2022-10-03 00:17:49 +0800

    Update api.php
    修复空行导致返回值可能为空的问题

--- api.php
+++ api.php
@@ -3,33 +3,27 @@ $sort = !empty($_GET['sort']) ? $_GET['sort'] : 'all';
 $num = !empty($_GET['num']) ? $_GET['num'] : 1;
 $type = $_GET['type'];
 $urls = [];
-$filename = './txt/' . $sort . '.txt';
-if (!file_exists($filename)) {
-    die('sort为空或文件不存在');
-}
+$i = 0;
 
-// 从文本获取链接
-$pics = file_get_contents($filename);
-$pics = explode(PHP_EOL, $pics);
-
-// 返回格式和数量
 $type = $num > 1 ? 'json' : $type;
 $num = $num > 100 ? 100 : $num;
 
-for ($i = 0; $i < $num; $i++) {
-    $url = $pics[array_rand($pics)];
-    $url = str_replace(array("\r","\n","\r\n"), '', $url);
-    array_push($urls, $url);
-}
+$arr = file('txt/' . $sort . '_raw.txt');
+$n = count($arr) - 1;
 
-// 返回指定格式
+do {
+    $x = rand(0, $n);
+    array_push($urls, $arr[$x]);
+    $i++;
+} while ($i < $num);
+$urls = str_replace(array("\r", "\n", "\r\n"), '', $urls);
+$urls = array_diff($urls, ['']);
+
 switch ($type) {
-
-        //JSON返回
     case 'json':
         header('Content-type:text/json');
         die(json_encode(['pics' => $urls], JSON_PRETTY_PRINT));
 
     default:
-        die(header("Location: $url"));
+        header("Location:" . $arr[$x]);
 }
